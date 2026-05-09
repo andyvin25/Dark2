00b3d433149902e4429194c04a1f6c73bb2d449f
andyvin <joviandy2020@gmail.com>
2026-05-09 11:58:20 +0700

    fix: remove bucket config

--- src/main/java/com/marketplace/Config/BucketConfig.java
+++ src/main/java/com/marketplace/Config/BucketConfig.java
@@ -1,12 +1,12 @@
-package com.marketplace.Config;
-
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Data
-@Configuration
-@ConfigurationProperties(prefix = "gcs")
-public class BucketConfig {
-    private String bucketName;
-}
+//package com.marketplace.Config;
+//
+//import lombok.Data;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Data
+//@Configuration
+//@ConfigurationProperties(prefix = "gcs")
+//public class BucketConfig {
+//    private String bucketName;
+//}
