0f999b8754357dcdab87d96dffdae73f36a51461
andyvin <joviandy2020@gmail.com>
2026-05-10 17:17:04 +0700

    add: java awt headless system property on main class

--- src/main/java/com/marketplace/MarketplaceApplication.java
+++ src/main/java/com/marketplace/MarketplaceApplication.java
@@ -1,7 +1,7 @@
 package com.marketplace;
 
 //import com.marketplace.Util.JjwtRuntimeHints;
-import com.marketplace.Config.AwtHints;
+//import com.marketplace.Config.AwtHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
@@ -9,12 +9,13 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.context.annotation.ReflectiveScan;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@ImportRuntimeHints(AwtHints.class)
+//@ImportRuntimeHints(AwtHints.class)
 @ReflectiveScan
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
         System.setProperty("spring.devtools.restart.enabled", "false");
+        System.setProperty("java.awt.headless", "true");
         SpringApplication.run(MarketplaceApplication.class, args);
     }
 
